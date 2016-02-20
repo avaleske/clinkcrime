91b18d1565a55deb97b175ba188b5c24f912c935
Austin Valeske <avaleske@gmail.com>
2016-02-19 23:31:17 -0800

    adding apps for scraper, soda api, and main page

--- clinkcrime/urls.py
+++ clinkcrime/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+# probs import the urls file in the future
+import main.views
 
 urlpatterns = [
     # Examples:
@@ -7,4 +9,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', main.views.index, name='index')
 ]
