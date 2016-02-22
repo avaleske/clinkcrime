e2058049e20aaf537af82fb496f482dedb76b96f
Austin Valeske <avaleske@gmail.com>
2016-02-22 11:50:28 -0800

    and updating the all_crime function

--- sodaapi/api.py
+++ sodaapi/api.py
@@ -1,9 +1,9 @@
-from django.conf import settings
-from itertools import imap, islice
-import pytz
 import time
 from datetime import datetime
+from itertools import imap, islice
 from sodapy import Socrata
+from django.conf import settings
+import pytz
 
 
 def get_crime_data():
@@ -13,9 +13,11 @@ def get_crime_data():
     limit = 20000
     offset = 0
     tries = 10
-    data = []
 
-    for i in xrange(tries):
+    # return the headers
+    yield ['cad_cdw_id', 'event_clearance_date', 'event_clearance_group']
+
+    for _ in xrange(tries):
         # sodapy throws an exception if we get a bad response
         # no need to handle it here, we'll catch it on the outside
         response = client.get(
@@ -29,19 +31,17 @@ def get_crime_data():
             limit=limit,
             offset=offset
         )
-
-        if len(response) == 1:  # just the headers
+        offset += len(response) - 1 # don't count the header
+        if len(response) == 1:  # if it's just the header
             break   # then there's nothing left to ask for
 
-        # if it's not the first page, then trim off the header row
+        # trim off the header row
         # using itertools so we don't make a shallow copy of the giant list
-        response_iter = islice(response, 0 if i == 0 else 1, None)
-
-        offset += len(response) - 1 # don't count the header
-        data.extend(response_iter)
-
+        response_iter = islice(response, 1, None)
+        for row in imap(_convert_row, response_iter):
+            yield row
     client.close()
-    return data
+
 
 def get_grouped_crime_data():
     # call crime data api
@@ -51,9 +51,10 @@ def get_grouped_crime_data():
     offset = 0
     tries = 10
 
+    # return the headers
     yield ['count', 'date', 'event_clearance_group']
 
-    for i in xrange(tries):
+    for _ in xrange(tries):
         # sodapy throws an exception if we get a bad response
         # no need to handle it here, we'll catch it on the outside
         response = client.get(
@@ -69,7 +70,7 @@ def get_grouped_crime_data():
             offset=offset
         )
         offset += len(response) - 1 # don't count the header
-        if len(response) == 1:  # just the headers
+        if len(response) == 1:  # if it's just the header
             break   # then there's nothing left to ask for
 
         # trim off the header row
@@ -78,6 +79,12 @@ def get_grouped_crime_data():
         for row in imap(_convert_grouped_row, response_iter):
             yield row
     client.close()
+
+# utils
+
+def _convert_row(row):
+    row[1] = _convert_date_to_js(row[1])
+    return row
 
 def _convert_grouped_row(row):
     row[1] = _convert_date_to_js(row[1])
