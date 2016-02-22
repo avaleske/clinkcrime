20430556be503f10df75f7517731d62ea32b1f80
Austin Valeske <avaleske@gmail.com>
2016-02-22 11:26:29 -0800

    now returns js timestamp, using an iterator instead of building the list 5 times

--- sodaapi/api.py
+++ sodaapi/api.py
@@ -1,7 +1,9 @@
 from django.conf import settings
-import itertools
+from itertools import imap, islice
+import pytz
+import time
+from datetime import datetime
 from sodapy import Socrata
-
 
 
 def get_crime_data():
@@ -32,8 +34,8 @@ def get_crime_data():
             break   # then there's nothing left to ask for
 
         # if it's not the first page, then trim off the header row
-        # using itertools so we don't make a shallowcopy of the giant list
-        response_iter = itertools.islice(response, 0 if i == 0 else 1, None)
+        # using itertools so we don't make a shallow copy of the giant list
+        response_iter = islice(response, 0 if i == 0 else 1, None)
 
         offset += len(response) - 1 # don't count the header
         data.extend(response_iter)
@@ -48,7 +50,8 @@ def get_grouped_crime_data():
     limit = 20000
     offset = 0
     tries = 10
-    data = []
+
+    yield ['count', 'date', 'event_clearance_group']
 
     for i in xrange(tries):
         # sodapy throws an exception if we get a bad response
@@ -56,25 +59,31 @@ def get_grouped_crime_data():
         response = client.get(
             settings.DATASET_ID,
             content_type="csv",
-            select='date_trunc_ymd(event_clearance_date) as day, event_clearance_group, count(*)',
+            select='date_trunc_ymd(event_clearance_date) as date, event_clearance_group, count(*)',
             where='event_clearance_date is not null and \
                     within_circle(incident_location, {0}, {1}, {2})'.format(
                         settings.CLINK_LAT, settings.CLINK_LON, settings.RADIUS),
-            group='day, event_clearance_group',
-            order='day DESC, count DESC',
+            group='date, event_clearance_group',
+            order='date DESC, count DESC',
             limit=limit,
             offset=offset
         )
-
+        offset += len(response) - 1 # don't count the header
         if len(response) == 1:  # just the headers
             break   # then there's nothing left to ask for
 
-        # if it's not the first page, then trim off the header row
-        # using itertools so we don't make a shallowcopy of the giant list
-        response_iter = itertools.islice(response, 0 if i == 0 else 1, None)
-
-        offset += len(response) - 1 # don't count the header
-        data.extend(response_iter)
-
+        # trim off the header row
+        # using itertools so we don't make a shallow copy of the giant list
+        response_iter = islice(response, 1, None)
+        for row in imap(_convert_grouped_row, response_iter):
+            yield row
     client.close()
-    return data
+
+def _convert_grouped_row(row):
+    row[1] = _convert_date_to_js(row[1])
+    return row
+
+def _convert_date_to_js(date_string):
+    dt = pytz.timezone('America/Los_Angeles').localize(
+        datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%S.000'), is_dst=True)
+    return int(time.mktime(dt.timetuple())) * 1000
